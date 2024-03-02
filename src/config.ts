b968c72ad002634820d7df8b95075cd17b054025
Jeongmin Lee <clcl6084@gmail.com>
2024-03-02 21:57:20 +0900

    chore: change the name of site and translation data update

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   website: "https://froggy1014.github.io/", // replace this with your deployed domain
   author: "이정민",
   desc: "안녕하세요. 주니어 프론트엔드 개발자 이정민입니다. 이 블로그는 개발자로서의 성장과 함께 제가 배운 것들과 회고를 기록하는 공간입니다.",
-  title: "에바니네",
+  title: "프로기네",
   ogImage: "evan-blog-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
