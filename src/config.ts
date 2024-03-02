cc82eb0e83bcec4f65e69750d8891f2fb62e7ed9
Jeongmin Lee <clcl6084@gmail.com>
2024-03-02 21:53:05 +0900

    docs: modify the Site info like og image or name of blog etc

--- src/config.ts
+++ src/config.ts
@@ -2,18 +2,18 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://froggy1014.github.io/", // replace this with your deployed domain
-  author: "Jeongmin Lee",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "Evan's Blog",
-  ogImage: "astropaper-og.jpg",
+  author: "이정민",
+  desc: "안녕하세요. 주니어 프론트엔드 개발자 이정민입니다. 이 블로그는 개발자로서의 성장과 함께 제가 배운 것들과 회고를 기록하는 공간입니다.",
+  title: "에바니네",
+  ogImage: "evan-blog-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
 export const LOCALE = {
-  lang: "en",
-  langTag: ["en-EN"],
+  lang: "ko",
+  langTag: ["ko-KR", "ko"],
 } as const;
 
 export const LOGO_IMAGE = {
