c4cc801ec95a4e5ed99f93f5e12a3286aadc00d7
Jeongmin Lee <clcl6084@gmail.com>
2024-03-06 03:43:03 +0900

    fix: add up the post count and remove console.log

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "프로기네",
   ogImage: "evan-blog-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 6,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
