5f1b759e0145af54444e9bbc96afe00f5b1d47e7
Jeongmin Lee <clcl6084@gmail.com>
2024-03-13 03:26:57 +0900

    ⚙️ Settings: add language specific sorting on postFilter util

--- src/utils/postFilter.ts
+++ src/utils/postFilter.ts
@@ -1,11 +1,18 @@
 import { SITE } from "@config";
 import type { CollectionEntry } from "astro:content";
+import i18next from "i18next";
 
 const postFilter = ({ data }: CollectionEntry<"blog">) => {
+  const isCurrentLanguage = data.language === i18next.language;
+
   const isPublishTimePassed =
     Date.now() >
     new Date(data.pubDatetime).getTime() - SITE.scheduledPostMargin;
-  return !data.draft && (import.meta.env.DEV || isPublishTimePassed);
+  return (
+    isCurrentLanguage &&
+    !data.draft &&
+    (import.meta.env.DEV || isPublishTimePassed)
+  );
 };
 
 export default postFilter;
