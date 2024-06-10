5ed4297453011ce8158df67169352cdf75d9e3cc
Jeongmin Lee <clcl6084@gmail.com>
2024-06-11 04:38:16 +0900

    🔨 settings: update organizationName and projectName on docusaurus.config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,12 +12,12 @@ const config: Config = {
   url: "https://froggy1014.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/evan-blog/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "evan-blog", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "froggy1014", // Usually your GitHub org/user name.
+  projectName: "froggy1014.github.io", // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: "throw",
