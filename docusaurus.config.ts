e7cd389dc8b7589c1d98a2a64ba403a678d47e45
Jeongmin Lee <clcl6084@gmail.com>
2024-06-15 01:07:32 +0900

    ✔️ chore: remove project card image from docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,8 +47,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
     navbar: {
       title: "Evan",
       items: [
