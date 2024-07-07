9cd629f1dff647518f2d644e303b2e74ed2e0e0b
Jeongmin Lee <clcl6084@gmail.com>
2024-07-07 20:00:25 +0900

    💅 styles: adjust max width of main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,22 +16,28 @@ function HomepageHeader() {
             height={200}
           />
         </div>
-        <div className="flex flex-col items-start pt-[20px] px-4 max-w-[100%] md:max-w-[70%]">
-          <Heading as="h3">안녕하세요. 프론트엔드 개발자 이정민입니다.</Heading>
-          <Heading as="h5" className="text-start leading-6">
-            저는 <span className="highlighted">블리딩 엣지</span>에 서서 새로운
-            기술을 배우고 이를 실제 프로젝트에 적용하는 것을 좋아하는
-            개발자입니다.
-            <span className="highlighted">대외 활동</span>
-            이나 <span className="highlighted">사이드 프로젝트</span>를 통해
-            새로운 지식을 빠르게 습득하고 반복적인 작업을 자동화하는 데 열정을
-            가지고 있습니다. 또한, <span className="highlighted">문서화</span>를
-            통해 지식을 체계적으로 정리하고 공유하는 것을 좋아하며, 이를 통해
-            팀의 협업을 원활하게 하고 프로젝트의 품질을 향상하는 데 기여하고
+        <div className="flex flex-col items-start pt-[20px] max-w-[100%] md:max-w-[80%]">
+          <Heading as="h3" className="break-words">
+            안녕하세요. 프론트엔드 개발자 이정민입니다.
+          </Heading>
+          <Heading as="h5" className="text-start leading-6 break-words">
+            저는 <span className="highlighted break-words	">블리딩 엣지</span>에
+            서서 새로운 기술을 배우고 이를 실제 프로젝트에 적용하는 것을
+            좋아하는 개발자입니다.
+            <span className="highlighted break-words">대외 활동</span>
+            이나{" "}
+            <span className="highlighted break-words">사이드 프로젝트</span>를
+            통해 새로운 지식을 빠르게 습득하고 반복적인 작업을 자동화하는 데
+            열정을 가지고 있습니다. 또한,{" "}
+            <span className="highlighted break-words">문서화</span>를 통해
+            지식을 체계적으로 정리하고 공유하는 것을 좋아하며, 이를 통해 팀의
+            협업을 원활하게 하고 프로젝트의 품질을 향상하는 데 기여하고
             있습니다. 궁극적으로, 지속적으로 학습하고 배운 바를 적용함으로써
             개발자 경험을 쌓고
-            <span className="highlighted">사회에 기여하는 개발자</span>로
-            성장하고 싶습니다.
+            <span className="highlighted break-words">
+              사회에 기여하는 개발자
+            </span>
+            로 성장하고 싶습니다.
           </Heading>
         </div>
       </div>
