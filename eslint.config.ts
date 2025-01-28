372fa4764e115741b4c1f0cdee6faae347085fef
Jeongmin Lee <clcl6084@gmail.com>
2025-01-29 04:27:47 +0900

    refactor(eslint): update eslint to latest

---
+++ eslint.config.ts
@@ -0,0 +1,43 @@
+import eslint from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
+import tseslint from "typescript-eslint";
+
+// @ts-ignore
+import hooksPlugin from "eslint-plugin-react-hooks";
+
+// ? https://typescript-eslint.io/getting-started#step-2-configuration
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+
+      "no-duplicate-imports": "off",
+    },
+  },
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": hooksPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs["jsx-runtime"].rules,
+      ...hooksPlugin.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+
+  // ignore files
+  {
+    ignores: ["**/*/dist/", "**/node_modules/", "*.config.*"],
+  },
+);
