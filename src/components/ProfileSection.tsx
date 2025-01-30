9a5c422b87e0951ac82638d38a37fb8c2b401892
Jeongmin Lee <clcl6084@gmail.com>
2025-01-31 04:39:30 +0900

    chore(icon): lower the size

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -21,30 +21,24 @@ export default function ProfileSection() {
     <section className="flex flex-col items-center p-4 md:p-6 lg:p-8 text-center">
       <Avatar className="w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 mb-2">
         <AvatarImage src={avatarUrl} alt={name} />
-        <AvatarFallback>{name.charAt(0)}</AvatarFallback>
+        <AvatarFallback className="bg-transparent" />
       </Avatar>
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-2">
         {name}
       </h1>
       <div className="flex gap-2 md:gap-3 lg:gap-4 mb-2">
         <IconLink
-          icon={
-            <GitHubLogoIcon className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
-          }
+          icon={<GitHubLogoIcon className="w-8 h-8 md:w-10 md:h-10" />}
           href="https://github.com/froggy1014"
           label="GitHub"
         />
         <IconLink
-          icon={
-            <InstagramLogoIcon className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
-          }
+          icon={<InstagramLogoIcon className="w-8 h-8 md:w-10 md:h-10" />}
           href="https://www.instagram.com/min6_6b"
           label="Instagram"
         />
         <IconLink
-          icon={
-            <LinkedInLogoIcon className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
-          }
+          icon={<LinkedInLogoIcon className="w-8 h-8 md:w-10 md:h-10" />}
           href="https://www.linkedin.com/in/jeongmin-lee-617a4a145"
           label="LinkedIn"
         />
