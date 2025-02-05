064e736b7582b5ef2b3a0f752c7082ed4850066e
Jeongmin Lee <clcl6084@gmail.com>
2025-02-06 02:11:16 +0900

    chore(component): add skeleton component

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -8,20 +8,21 @@ import {
 } from "@radix-ui/react-icons";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import IconLink from "./ui/iconLink";
+import { Skeleton } from "./ui/skeleton";
 
 export default function ProfileSection() {
   const themeConfig = useThemeConfig();
 
   const name = "Jeongmin Lee";
   const avatarUrl = useBaseUrl(themeConfig.image || "/img/avatar.webp");
-  const bio =
-    "블리딩엣지에 서서 꾸준한 페이스로 완주까지 하고 싶은 프론트엔드 개발자 이정민입니다.";
 
   return (
     <section className="flex flex-col items-center p-4 md:p-6 lg:p-8 text-center">
       <Avatar className="w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 mb-2">
         <AvatarImage src={avatarUrl} alt={name} />
-        <AvatarFallback className="bg-transparent" />
+        <AvatarFallback>
+          <Skeleton className="w-full h-full" />
+        </AvatarFallback>
       </Avatar>
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-2">
         {name}
@@ -44,7 +45,9 @@ export default function ProfileSection() {
         />
       </div>
       <p className="max-w-md md:max-w-lg text-gray-600 text-base md:text-lg lg:text-xl">
-        {bio}
+        블리딩엣지에 서서 꾸준한 페이스로 완주까지 하고 싶은 프론트엔드 개발자{" "}
+        <span className="font-bold text-pretty dark:text-white">이정민</span>
+        입니다.
       </p>
     </section>
   );
