074b9c1bf4958645b07c6282da7453b282184dff
Jeongmin Lee <clcl6084@gmail.com>
2025-02-17 03:32:21 +0900

    docs(docker): posting about volume

--- sidebars.ts
+++ sidebars.ts
@@ -18,6 +18,7 @@ const sidebars: SidebarsConfig = {
           items: [
             "study/docker-kubernetes/overview",
             "study/docker-kubernetes/image-and-container",
+            "study/docker-kubernetes/volume",
           ],
         },
         "study/swr",
